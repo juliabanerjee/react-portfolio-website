b1fa0706a81283d79196bce61d1bc3d24286754b
Julia Banerjee <juliabanerjee27@gmail.com>
2022-01-26 15:06:52 +0000

    more set-up

---
+++ src/components/HomePage/Homepage.jsx
@@ -0,0 +1,16 @@
+import "./HomePage.scss";
+
+const HomePage = () =>{
+
+
+
+
+  return(
+    <section className="home-page">
+<h2 className="home-page__h2">Hello my name is Julia and I'm a Software Developer</h2>
+
+    </section>
+  )
+};
+
+export default HomePage;
