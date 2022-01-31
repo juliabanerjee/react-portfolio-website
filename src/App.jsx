313e275bda71df410c0f46c4251c8fa1cc38f376
Julia Banerjee <juliabanerjee27@gmail.com>
2022-01-31 10:28:23 +0000

    routing basic set up done for desktop nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
 import './App.scss';
-import HomePage from "./containers/HomePage/Homepage";
+import HomePage from "./containers/HomePage/HomePage";
+import AboutPage from "./containers/AboutPage/Aboutpage";
+import ExperiencePage from "./containers/ExperiencePage/ExperiencePage";
+import ProjectPage from "./containers/ProjectPage/ProjectPage";
+import ContactPage from "./containers/ContactPage/ContactPage";
+
 
 import {BrowserRouter as Router, Routes, Route} from 
 "react-router-dom";
+import DesktopNav from './components/DesktopNav/DesktopNav';
 
 function App() {
   return (
+    <>
+    <DesktopNav/>
     <Router> 
     <div className="App">
       <Routes>
-<Route path ="/" element={HomePage}/>
+        <Route path ="/" element={HomePage}/>
       </Routes>
+      <Routes>
+        <Route path ="/about" element={AboutPage}/>
+      </Routes>
+      <Routes>
+        <Route path ="/experience" element={ExperiencePage}/>
+      </Routes>
+      <Routes>
+        <Route path ="/projects" element={ProjectPage}/>
+      </Routes>
+      <Routes>
+        <Route path ="/contact" element={ContactPage}/>
+      </Routes>
+      
       
     </div>
     </Router>
+    </>
   );
 }
 
---
+++ src/components/DesktopNav/DesktopNav.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+
+const DesktopNav = () =>{
+
+return <div>
+  <nav>
+    <Link to="/" className="desktop-nav__menu-item">Home</Link>
+    <Link to="/about" className="desktop-nav__menu-item">About Me</Link>
+    <Link to="/experience" className="desktop-nav__menu-item">Experience</Link>
+    <Link to="/projects" className="desktop-nav__menu-item">My Projects</Link>
+    <Link to="/contact" className="desktop-nav__menu-item">Contact</Link>
+  </nav>
+</div>
+
+}
+export default DesktopNav
